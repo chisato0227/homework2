83115dd4673577cb29f6e20d6ff69d910ecca5eb
東千里 <>
2019-12-09 18:38:18 +0900

    js追加 デザイン変更、ページ分け

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,14 @@
 $(function(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    $(function(){
+        // ▼マウスが載ったらサブメニューを表示
+        $("ul.menubar li").mouseenter(function(){
+           $(this).siblings().find("ul").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
+           $(this).children().slideDown(150);     // 自分のサブメニューを表示する。
+        });
+        // ▼どこかがクリックされたらサブメニューを消す
+        $('html').click(function() {
+           $('ul.menubar ul').slideUp(150);
+        });
+     });
 })
